27f9feae3302ea81f5be726faef0596a267a59e9
tommycordes94 <tomalexandercordes@gmail.com>
2022-02-06 14:19:23 +0100

    including instructions on how to use git as a cloud service

--- GITtest.R
+++ GITtest.R
@@ -3,7 +3,35 @@
 #####################################
 
 get_yesterday <- function() {
-  return(Sys.Date() - 1)
+  x <- (Sys.Date() - 1)
+  return(x)
 }
 
-print(get_yesterday())
+print(x)
+
+####### ANLEITUNG: Zwischen comitteten Versionen springen ####### 
+# git - Diff - history - copy the SHA
+# open a new terminal via tools tab and then new terminal
+# type: git reset --hard SHA
+
+
+####### ANLEITUNG: Git als Cloud speicher ####### 
+install.packages("usethis")
+library(usethis)
+?use_github
+
+# Personal access token (PAT) generieren unter...
+create_github_token()
+# Copy the PAT 
+edit_r_environ()
+
+#insert: GITHUB_PAT = 'PAT'
+# and restart R Project
+# reload the usethis package 
+use_github(protocol = 'https', 
+           auth_token = Sys.getenv("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"))
+
+
+
+
+
