54bd714b4fe141cca5f61d15cec60ae8ff49a17b
tommycordes94 <tomalexandercordes@gmail.com>
2022-02-06 13:19:55 +0100

    Jucheeey, this is my first commit! :)

---
+++ GITtest.R
@@ -0,0 +1,9 @@
+#####################################
+#### T E S T # T E S T # T E S T ####
+#####################################
+
+get_yesterday <- function() {
+  return(Sys.Date() - 1)
+}
+
+print(get_yesterday())
